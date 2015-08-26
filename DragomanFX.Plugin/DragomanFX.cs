d4f94f72627e400d729a8625bee2e9d635fc4110
Denikson <mrdenikson@gmail.com>
2015-08-27 00:52:35 +0300

    Initial commit. Added plug-in body and logging.

---
+++ DragomanFX.Plugin/DragomanFX.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using DragomanFX.Plugin.Utils;
+using UnityInjector;
+using UnityInjector.Attributes;
+
+namespace DragomanFX.Plugin
+{
+    [PluginName("DragomanFX"), PluginVersion("Alpha 0.1")]
+    public class DragomanFX : PluginBase
+    {
+        private static string path;
+
+        public static string FXPath => path ?? (path = GetFXPath());
+
+        public void Awake()
+        {
+            DontDestroyOnLoad(this);
+            Logger.LogToFile = true;
+            Logger.LogLine("DragomanFX loaded!");
+        }
+
+        public void OnDestroy()
+        {
+            Logger.LogLine("Closing DragomanFX!");
+            if (Logger.LogToFile)
+            {
+                Logger.LogLine("Saving the log...");
+                Logger.SaveLog();
+            }
+        }
+
+        private static string GetFXPath()
+        {
+            string logPath = Path.Combine(Environment.CurrentDirectory, "DragomanFX");
+            Logger.LogLine($"DragomanFX Data Path: {logPath}");
+            if (Directory.Exists(logPath))
+                return logPath;
+            try
+            {
+                Directory.CreateDirectory(logPath);
+            }
+            catch (Exception)
+            {
+                Logger.LogLine(LogLevel.Error, "Failed to create DragomanFX directory!");
+                return null;
+            }
+
+            return logPath;
+        }
+
+    }
+}
---
+++ DragomanFX.Plugin/Utils/Logger.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+
+namespace DragomanFX.Plugin.Utils
+{
+    public struct LogLevel
+    {
+        public static LogLevel Info = new LogLevel(ConsoleColor.Blue, "INFO");
+        public static LogLevel Warning = new LogLevel(ConsoleColor.Yellow, "WARNING");
+        public static LogLevel Error = new LogLevel(ConsoleColor.Red, "ERROR");
+
+        public ConsoleColor Color { get; }
+        public string Name { get; }
+
+        public LogLevel(ConsoleColor color, string name)
+        {
+            Color = color;
+            Name = name;
+        }
+    }
+
+    public static class Logger
+    {
+        private const string TAG = "DragomanFX";
+        private const string DEBUG_FILE_NAME = "dragoman_fx_log.log";
+        private const ConsoleColor TAG_COLOR = ConsoleColor.DarkGreen;
+
+        private static TextWriter logFile;
+
+        public static bool LogToFile
+        {
+            get
+            {
+                return logFile != null;
+            }
+            set
+            {
+                if (value && logFile == null)
+                {
+                    try
+                    {
+                        logFile = File.CreateText(Path.Combine(DragomanFX.FXPath, DEBUG_FILE_NAME));
+                    }
+                    catch (Exception)
+                    {
+                        logFile = null;
+                        LogLine(LogLevel.Error, "Failed to create the log file!");
+                    }
+                }
+                else if (!value) SaveLog();
+            }
+        }
+
+        public static void LogLine(string msg)
+        {
+            string tag = $"{TAG}({DateTime.Now.ToString("hh:mm:ss:ff")}): ";
+
+            if (LogToFile)
+            {
+                logFile.WriteLine(tag + msg);
+                logFile.Flush();
+            }
+
+            Console.ForegroundColor = TAG_COLOR;
+            Console.Write(tag);
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine(msg);
+        }
+
+        public static void LogLine(LogLevel level, string msg)
+        {
+            string tag = $"{TAG}[{level.Name}]({DateTime.Now.ToString("hh:mm:ss:ff")}): ";
+
+            if (LogToFile)
+            {
+                logFile.WriteLine(tag + msg);
+                logFile.Flush();
+            }
+
+            Console.ForegroundColor = level.Color;
+            Console.Write(tag);
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine(msg);
+        }
+
+        public static void SaveLog()
+        {
+            if (!LogToFile)
+                return;
+
+            logFile.Flush();
+            logFile.Close();
+            logFile.Dispose();
+            logFile = null;
+        }
+    }
+}
